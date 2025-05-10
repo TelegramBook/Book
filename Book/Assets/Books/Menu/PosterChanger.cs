bb265e0ce44d8ce07673f8434fdb88644c3b04de
Ivan A. <Ivan@Antonishin.com>
2025-05-11 00:18:23 +0300

    add main menu

---
+++ Book/Assets/Books/Menu/PosterChanger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Books.Menu 
+{
+    public class PosterChanger : MonoBehaviour
+    {
+        [SerializeField] private Transform _targetPoint;
+        [SerializeField] private CanvasGroup _detailGroup;
+
+        private void Update()
+        {
+            var t = (Mathf.Abs(_targetPoint.position.x - transform.position.x) / (Screen.width * 0.33f));
+            transform.localScale = Vector3.one * Mathf.Lerp(1f, 0.85f, t);
+            _detailGroup.alpha = Mathf.Lerp(1f, 0f, t);
+        }
+    }
+}
+
